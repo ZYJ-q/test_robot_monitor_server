b747b1d3a1d8eb2570bac496ce1583d7d7903fc9
ZYJ-q <2979049853@qq.com>
2023-07-14 17:54:16 +0800

    p

--- src/actors/adapters/bybit/parase.rs
+++ src/actors/adapters/bybit/parase.rs
@@ -37,7 +37,7 @@ pub async fn get_account_bybit(
                 continue;
             } else {
                 let symbol = objs.get("coin").unwrap().as_str().unwrap();
-                if let Some(data) = http_api.get_open_orders("spot").await{
+                if let Some(data) = http_api.position("linear").await{
                   
                   let v: Value = serde_json::from_str(&data).unwrap();
                   let open_orders_obj = v.as_object().unwrap();
